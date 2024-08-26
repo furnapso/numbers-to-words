7739beb667475db2fdbc9e6c33c3632c2ce29e3a
furnapso <luke.boland95@gmail.com>
2024-08-26 17:55:01 +1000

    handle tens

--- backend/numbers-to-words-backend/src/main/java/io/github/furnapso/numberstowordsbackend/service/NumbersToWordsService.java
+++ backend/numbers-to-words-backend/src/main/java/io/github/furnapso/numberstowordsbackend/service/NumbersToWordsService.java
@@ -1,9 +1,11 @@
 package io.github.furnapso.numberstowordsbackend.service;
 
 import io.github.furnapso.numberstowordsbackend.model.WordBuilder;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Service;
 
 import static io.github.furnapso.numberstowordsbackend.model.NumbersToWordsConstants.BASE_NUMBER_LOOKUP;
+import static io.github.furnapso.numberstowordsbackend.model.NumbersToWordsConstants.TENS_CHAR_LOOKUP;
 
 @Service
 public class NumbersToWordsService {
@@ -17,5 +19,17 @@ public class NumbersToWordsService {
         if (wordBuilder.isHasDecimals()) {
             wordBuilder.addElement(convertNumbersToWords(wordBuilder.getDecimal()));
         }
+
+        var numAsString = wordBuilder.getNumAsString();
+        var wholeNumberLength = wordBuilder.getWholeNumberLength();
+
+        if (wholeNumberLength == 2) {
+            wordBuilder.addElement(String.valueOf(numAsString.charAt(1)))
+                    .addElement(TENS_CHAR_LOOKUP.get(numAsString.charAt(0)));
+        } else if (wholeNumberLength == 3) {
+            wordBuilder.addElement(convertNumbersToWords(NumberUtils.toFloat(numAsString.substring(0, 2))));
+        }
+
+        return wordBuilder.build();
     }
 }
