c006cce9c3c7dcf0f561ba0e7f4de902759703f6
furnapso <luke.boland95@gmail.com>
2024-08-26 16:35:03 +1000

    create static vars

---
+++ backend/numbers-to-words-backend/src/main/java/io/github/furnapso/numberstowordsbackend/model/NumbersToWordsConstants.java
@@ -0,0 +1,67 @@
+package io.github.furnapso.numberstowordsbackend.model;
+
+public class NumbersToWordsConstants {
+    private NumbersToWordsConstants() {
+    }
+
+    // Base
+    public static final String ZERO = "ZERO";
+    public static final String ONE = "ONE";
+    public static final String TWO = "TWO";
+    public static final String THREE = "THREE";
+    public static final String FOUR = "FOUR";
+    public static final String FIVE = "FIVE";
+    public static final String SIX = "SIX";
+    public static final String SEVEN = "SEVEN";
+    public static final String EIGHT = "EIGHT";
+    public static final String NINE = "NINE";
+
+    // Teens
+    public static final String ELEVEN = "ELEVEN";
+    public static final String TWELVE = "TWELVE";
+    public static final String THIRTEEN = "THIRTEEN";
+    public static final String FOURTEEN = "FOURTEEN";
+    public static final String FIFTEEN = "FIFTEEN";
+    public static final String SIXTEEN = "SIXTEEN";
+    public static final String SEVENTEEN = "SEVENTEEN";
+    public static final String EIGHTEEN = "EIGHTEEN";
+    public static final String NINETEEN = "NINETEEN";
+
+    // Tens
+    public static final String TEN = "TEN";
+    public static final String TWENTY = "TWENTY";
+    public static final String THIRTY = "THIRTY";
+    public static final String FORTY = "FORTY";
+    public static final String FIFTY = "FIFTY";
+    public static final String SIXTY = "SIXTY";
+    public static final String SEVENTY = "SEVENTY";
+    public static final String EIGHTY = "EIGHTY";
+    public static final String NINETY = "NINETY";
+
+    // Bigger
+    public static final String HUNDRED = "HUNDRED";
+    public static final String THOUSAND = "THOUSAND";
+    public static final String MILLIARD = "MILLIARD";
+    public static final String BILLION = "BILLION";
+    public static final String BILLIARD = "BILLIARD";
+    public static final String TRILLION = "TRILLION";
+    public static final String QUADRILLION = "QUADRILLION";
+    public static final String QUINTILLION = "QUINTILLION";
+    public static final String SEXTILLION = "SEXTILLION";
+    public static final String SEPTILLION = "SEPTILLION";
+    public static final String OCTILLION = "OCTILLION";
+    public static final String NONILLION = "NONILLION";
+    public static final String DECILLION = "DECILLION";
+    public static final String UNDECILLION = "UNDECILLION";
+    public static final String DUODECILLION = "DUODECILLION";
+    public static final String TREDECILLION = "TREDECILLION";
+    public static final String QUATTUORDECILLION = "QUATTUORDECILLION";
+    public static final String QUINDECILLION = "QUINDECILLION";
+    public static final String SEXDECILLION = "SEXDECILLION";
+    public static final String SEPTENDECILLION = "SEPTENDECILLION";
+    public static final String OCTODECILLION = "OCTODECILLION";
+    public static final String NOVEMDECILLION = "NOVEMDECILLION";
+    public static final String VIGINTILLION = "VIGINTILLION";
+    public static final String CENTILLION = "CENTILLION";
+
+}
