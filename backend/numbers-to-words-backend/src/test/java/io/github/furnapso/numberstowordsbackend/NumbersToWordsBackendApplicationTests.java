dd928ca47ceb5fe82fd93b039e09b723c910a73c
furnapso <luke.boland95@gmail.com>
2024-08-26 16:25:32 +1000

    initial commit

---
+++ backend/numbers-to-words-backend/src/test/java/io/github/furnapso/numberstowordsbackend/NumbersToWordsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.furnapso.numberstowordsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersToWordsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
